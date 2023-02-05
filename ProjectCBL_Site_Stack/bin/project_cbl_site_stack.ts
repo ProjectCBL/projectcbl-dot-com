e4963e2eaf11a7da57cf819509c3a0f3511ad3df
projectcbl <projectxcbl@gmail.com>
2023-02-04 19:00:43 -0500

    Changed to typescript

---
+++ ProjectCBL_Site_Stack/bin/project_cbl_site_stack.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import * as config from '../config.json';
+import { ProjectCblSiteStackStack } from '../lib/project_cbl_site_stack-stack';
+
+const app = new cdk.App();
+new ProjectCblSiteStackStack(app, 'ProjectCblSiteStackStack', {
+    env: {
+        account: config.account,
+        region: config.region
+    }
+});
---
+++ ProjectCBL_Site_Stack/test/project_cbl_site_stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ProjectCblSiteStack from '../lib/project_cbl_site_stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/project_cbl_site_stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ProjectCblSiteStack.ProjectCblSiteStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
