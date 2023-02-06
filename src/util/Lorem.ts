5b85775db73b9b23d4b0bca0831c032e8817b6b1
projectcbl <projectxcbl@gmail.com>
2023-02-06 00:38:07 -0500

    Added a util file for holding lorem filler text

---
+++ src/util/Lorem.ts
@@ -0,0 +1,37 @@
+interface LoremStructure {
+    small?: string;
+    medium?: string;
+    large?: string;
+}
+
+const Lorem: LoremStructure = {
+    small: `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+    do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+    qui officia deserunt mollit anim id est laborum.
+    `,
+
+    medium: `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna aliqua. Vel turpis nunc eget 
+    lorem dolor sed viverra ipsum nunc. Iaculis nunc sed augue lacus. Id 
+    consectetur purus ut faucibus. Ullamcorper morbi tincidunt ornare massa 
+    eget egestas purus viverra. Egestas sed tempus urna et. Varius vel pharetra 
+    vel turpis nunc eget lorem dolor sed. Lacus sed viverra tellus in hac. 
+    Malesuada pellentesque elit eget gravida. Est sit amet facilisis magna etiam. 
+    Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. 
+    Sed tempus urna et pharetra pharetra. Bibendum arcu vitae elementum curabitur. 
+    Adipiscing elit pellentesque habitant morbi tristique senectus et netus. Pulvinar 
+    mattis nunc sed blandit libero volutpat sed cras. Maecenas volutpat blandit aliquam 
+    etiam erat. Magnis dis parturient montes nascetur ridiculus mus mauris. Nunc pulvinar 
+    sapien et ligula. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. 
+    Eros in cursus turpis massa tincidunt.
+    `
+}
+
+export default Lorem;
+
